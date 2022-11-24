5f6214d1e455fad70ccc354118ae6411c26a1fcc
mlhoutel <mlhoutel@gmail.com>
2022-11-24 17:31:55 +0100

    Add prettier file and format project

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const YAML = require("yaml");
-const path = require("path");
+const YAML = require('yaml');
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const { findRequirements, meetsRequirement } = require("./lib/parsing");
-const { readFile } = require("./lib/system");
+const {findRequirements, meetsRequirement} = require('./lib/parsing');
+const {readFile} = require('./lib/system');
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || "0.0.0.0";
-const SITE_ID = process.env.SITE_ID || "Defaut";
+const HOST = process.env.HOST || '0.0.0.0';
+const SITE_ID = process.env.SITE_ID || 'Defaut';
 
 const SITES_PATH = path.join(
   __dirname,
-  process.env.SITES_PATH || "/assets/sites.json"
+  process.env.SITES_PATH || '/assets/sites.json'
 );
 
 const TEMPLATE_PATH = path.join(
   __dirname,
-  process.env.TEMPLATE_PATH || "/assets/template.json"
+  process.env.TEMPLATE_PATH || '/assets/template.json'
 );
 const CONFIG_PATH = path.join(
   __dirname,
-  process.env.CONFIG_PATH || "/assets/deployment.yaml"
+  process.env.CONFIG_PATH || '/assets/deployment.yaml'
 );
 const RESOURCES_PATH = path.join(
   __dirname,
-  process.env.RESOURCES_PATH || "/resources/resources_A.json"
+  process.env.RESOURCES_PATH || '/resources/resources_A.json'
 );
 
 const app = express();
@@ -41,24 +41,24 @@ app.use(
 );
 const template = JSON.parse(readFile(TEMPLATE_PATH));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const config = YAML.parse(readFile(CONFIG_PATH));
-  const { outputAsList, outputAsTree } = findRequirements(
+  const {outputAsList, outputAsTree} = findRequirements(
     config,
     template.definitions
   );
 
   res.send(
-    "<pre>" +
+    '<pre>' +
       JSON.stringify(outputAsList, null, 2) +
-      "</pre>" +
-      "<pre>" +
+      '</pre>' +
+      '<pre>' +
       JSON.stringify(outputAsTree, null, 2) +
-      "</pre>"
+      '</pre>'
   );
 });
 
-app.post("/verify", async (req, res) => {
+app.post('/verify', async (req, res) => {
   const config = YAML.parse(req.body.config);
   const sites_id_list = JSON.parse(req.body.sites) || [];
 
@@ -66,16 +66,13 @@ app.post("/verify", async (req, res) => {
 
   // get required fields in the user config
 
-  const { outputAsList, error } = findRequirements(
-    config,
-    template.definitions
-  );
+  const {outputAsList, error} = findRequirements(config, template.definitions);
   if (error) {
     return res.status(400).send(error);
   }
-  const splittedRequirements = outputAsList.map((r) => ({
+  const splittedRequirements = outputAsList.map(r => ({
     value: r.value,
-    path: r.path.split("."),
+    path: r.path.split('.'),
     origin: [],
   }));
 
@@ -83,7 +80,7 @@ app.post("/verify", async (req, res) => {
 
   function checkAllRequirements(requirements, ressources, siteId) {
     for (const requirement of requirements) {
-      const requirement_copy = { ...requirement };
+      const requirement_copy = {...requirement};
       // We check if the requirement is in the resource
       if (meetsRequirement(requirement_copy, ressources, siteId)) {
         requirement.origin.push(siteId); // we fill the requirement with the resource origin
@@ -105,10 +102,10 @@ app.post("/verify", async (req, res) => {
   }
 
   // We don't need to query distant db
-  if (splittedRequirements.every((p) => p.origin.length > 0)) {
-    const requirements = splittedRequirements.map((s) => ({
+  if (splittedRequirements.every(p => p.origin.length > 0)) {
+    const requirements = splittedRequirements.map(s => ({
       ...s,
-      path: s.path.join("."),
+      path: s.path.join('.'),
     }));
 
     res.send(requirements);
@@ -117,8 +114,8 @@ app.post("/verify", async (req, res) => {
 
   // recovers the sites urls
   const sites_config = JSON.parse(readFile(SITES_PATH)).sites;
-  const sites = sites_id_list.map((site_id) => {
-    const site = sites_config.find((c) => c.id === site_id);
+  const sites = sites_id_list.map(site_id => {
+    const site = sites_config.find(c => c.id === site_id);
 
     if (!site) {
       throw new Error(`${site_id} not found`);
@@ -128,11 +125,11 @@ app.post("/verify", async (req, res) => {
   });
 
   for (const site of sites) {
-    const response = await fetch(site.url + "/resources");
+    const response = await fetch(site.url + '/resources');
     const ressources = await response.json();
     const ressourcesInSite = ressources.local;
     for (const requirement of splittedRequirements) {
-      const requirement_copy = { ...requirement };
+      const requirement_copy = {...requirement};
       // We check if the requirement is in the resource
       if (meetsRequirement(requirement_copy, ressourcesInSite, site.id)) {
         requirement.origin.push(site.id); // we fill the requirement with the resource origin
@@ -141,15 +138,15 @@ app.post("/verify", async (req, res) => {
   }
 
   // Reconstruct path from splitted
-  const requirements = splittedRequirements.map((s) => ({
+  const requirements = splittedRequirements.map(s => ({
     ...s,
-    path: s.path.join("."),
+    path: s.path.join('.'),
   }));
 
   res.send(requirements);
 });
 
-app.get("/resources", (req, res) => {
+app.get('/resources', (req, res) => {
   res.send(readFile(RESOURCES_PATH));
 });
 
